db252cc8cc6b98f29613b44459d8ee86420996cd
Hana <hanaadhami01@gmail.com>
2019-11-19 18:24:06 -0800

    adding the raw files

---
+++ Intern.test.js
@@ -0,0 +1,31 @@
+const Intern = require("../lib/Intern");
+
+function Intern (name, id, title, getName, getId, getEmail, getRole, school, getSchool) {
+  this.name = name
+  this.id = id
+  this.title = title
+  this.getName = getName()
+  this.getId = getId()
+  this.getEmail = getEmail()
+  this.getRole = getRole()
+  this.school = school
+  this.getSchool = getSchool()
+}
+
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
---
+++ Manager.test.js
@@ -0,0 +1,31 @@
+const Manager = require("../lib/Manager");
+
+function Manager (name, id, title, getName, getId, getEmail, getRole, officeNumber, getRole) {
+  this.name = name
+  this.id = id
+  this.title = title
+  this.getName = getName()
+  this.getId = getId()
+  this.getEmail = getEmail()
+  this.getRole = getRole()
+  this.officeNumber = officeNumber
+  this.getRole = getRole
+}
+
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
